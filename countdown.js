f7a0ca0f50e8992baaf8511e47356bbfb1632c58
Skyreep <skyreep@gmail.com>
2019-04-16 18:10:50 -0400

    Add files via upload

--- countdown.js
+++ countdown.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 30, 2019 13:00:00").getTime();
+var countDownDate = new Date("Apr 30, 2019 10:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
